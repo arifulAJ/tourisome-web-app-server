52659ef022fe44805beec7661095d150d2519c28
arifulislam10111999 <arifulislam10111999@gmail.com>
2022-01-29 07:22:02 +0600

    gitignore file

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const  app=express();
-const port=process.env.PORT || 5000 ;
-// user= tourisome-web 
-// pass= 7sbZNjzrB2KMzwAY
-
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://tourisome-web:7sbZNjzrB2KMzwAY@cluster0.lreh2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const  app=express();
+require('dotenv').config()
+const port=process.env.PORT || 5000 ;
+
+
+
+const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASS@cluster0.lreh2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  console.log("hitting the  database");
+  console.log("hitting the  database colling the ");
   // perform actions on the collection object
   client.close();
 });
